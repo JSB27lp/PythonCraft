e7ffc9f870f374e354544aafc4357802d42dfb5d
JSB27lp <1conscienceofficiel@gmail.com>
2025-03-29 08:49:36 +0100

    Implement stealing mechanism in Thief class to transfer minerals between characters

--- Character/Thief.py
+++ Character/Thief.py
@@ -5,5 +5,22 @@ class Thief(Character):
     
         super().__init__(x,y,white,type,main)
 
+    def steal(self,world):
+        for character in world.characters:
+            if round(character.x) == round(self.x) and round(character.y) == round(self.y) :
+                if self.white and not character.white:
+                    self.blue_minerals+=character.blue_minerals
+                    self.pink_minerals+=character.pink_minerals
+
+                    character.blue_minerals = 0
+                    character.pink_minerals = 0
+
+                if not self.white and character.white:
+                    self.blue_minerals+=character.blue_minerals
+                    self.pink_minerals+=character.pink_minerals
+
+                    character.blue_minerals = 0
+                    character.pink_minerals = 0
+
     def action(self,world):
-        pass
+        self.steal(world)
